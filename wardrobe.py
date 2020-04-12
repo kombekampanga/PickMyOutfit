d68ac56f0ce7c3523fda97858f3af6dfe92e101d
kombekampanga <kombekampanga@users.noreply.github.com>
2020-04-12 21:57:34 +1200

    Renamed functions
    Renamed functions because python doesn't like names with capitals
    
    Starting to try interating through different images:
    - created two lists with 4 clothing items each - one for tops and one for bottoms
    - Created index variables to track which top and bottom we are up to in the list (for when we press next and previous)
    - Changed call to 'create_photo' function so it calls for the first top and bottom item in the respective lists

--- wardrobe.py
+++ wardrobe.py
@@ -53,12 +53,18 @@ WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 700
 
 #list of image names
-image_names = ["top1.jpg", "bottom1.jpg"]
+top_image_names = ["top1.jpg", "top2.jpg", "top3.jpg", "top4.jpg"]
+bottom_image_names = ["bottom1.jpg","bottom2.jpg", "bottom3.jpg", "bottom4.jpg"]
 # frame (that displays the top and bottom takes up 30% of the page in the centre
 # with 35% remaining either side of the frame
 FRAME_WIDTH = WINDOW_WIDTH * 0.3
 FRAME_HEIGHT = WINDOW_HEIGHT * 0.3
 PERCENTAGE_EITHER_SIDE_OF_FRAME = 0.35
+
+# Index numbers to keep track of which top/bottom item we are looking at
+# So when we click next and previous we can move through the items
+top_index = 0
+bottom_index = 0
 
 
 # Wardrobe class that contains the window for the clothes to be displayed
@@ -68,14 +74,18 @@ class WardrobeApp:
         self.root = root
 
         # Create the details for the window
-        self.CreateFrame()
-        self.CreateButtons()
-       # self.CreateLabels()
-        # Upload the top and the bottom into the frame
-        for image_name in image_names:
-            self.CreatePhoto(image_name)
+        self.create_frame()
+        self.create_buttons()
+        # self.CreateLabels()
 
-    def CreateFrame(self):
+        # Upload the initial top and the bottom picture into the frame (indices initially = 0)
+        self.create_photo(top_image_names[top_index])
+        self.create_photo(bottom_image_names[bottom_index])
+
+        #
+
+
+    def create_frame(self):
         # add title to window
         self.root.title(WINDOW_TITLE)
         # change the size of the window
@@ -91,10 +101,10 @@ class WardrobeApp:
         self.frame_bottom = tk.Frame(self.root, bg='#80c1ff')
         self.frame_bottom.place(relx=PERCENTAGE_EITHER_SIDE_OF_FRAME, rely=0.5, width=FRAME_WIDTH, height=FRAME_HEIGHT)
 
-    def CreateButtons(self):
+    def create_buttons(self):
         # create a previous and next buttons for each item and add them to the root
 
-        # buttoms for tops
+        # buttons for tops
         self.button_prev_top = tk.Button(self.root, text="Prev", bg='pink')
         self.button_prev_top.place(relx=0.25, rely=0.25, relwidth=0.08, relheight=0.05)
 
@@ -112,7 +122,7 @@ class WardrobeApp:
         self.button_random_outfit = tk.Button(self.root, text="Random Outfit", bg='light green')
         self.button_random_outfit.place(relx=0.05, rely=0.05)
 
-    def CreateLabels(self):
+    def create_labels(self):
         # Create label for the tops area
         self.label_top = tk.Label(self.frame_top, text="Tops", bg='white')
         self.label_top.place(relx=0.3, rely=0.02, relwidth=0.4, height=0.1)
@@ -121,7 +131,7 @@ class WardrobeApp:
         self.label_bottom = tk.Label(self.frame_bottom, text="Bottoms", bg='white')
         self.label_bottom.place(relx=0.3, rely=0.02, relwidth=0.4, relheight=0.1)
 
-    def CreatePhoto(self, image_name):
+    def create_photo(self, image_name):
         # load the image in
         load = Image.open(image_name)
         # Resize the image so it fits in the frame
@@ -143,7 +153,11 @@ class WardrobeApp:
         # Place the image into the frame
         image_label.place(x=0, y=0)
 
-# def CreateDrobBoxes(self):
+
+    def next_photo(self):
+
+
+# def CreateDropBoxes(self):
 
 # Dropbox for the Weather - Hot, Warm, Cold and Windy, Cold and Rainy
 # Dropbox for occasion - Work, House Party, Town, 21st ish, Everyday/Errands, Uni, Family/Church
